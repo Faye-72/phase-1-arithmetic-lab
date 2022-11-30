f499b0301b1266d8131194609c4f7e03182db507
Faith Mitei <faithmitey@gmail.com>
2022-11-30 16:45:31 +0300

    attempt on arithmetic  quiz

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+//quiz1
+
+let num1 = 31 ;
+let num2 = 2;
+multiply = (num1) * (num2);
+
+//quiz2
+
+ random = Math.floor(Math.random()* 10 )+1
+
+ 
+ //quiz3
+ num3 = 9;
+
+ mod = num3 % 5;
+
+ //quiz4
+
+ max = Math.max(20);
+
+ 
