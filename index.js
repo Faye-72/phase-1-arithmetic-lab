fb6c22f140d9f2caaed9f80692974fc905d2ddce
faith mitei <faithmitey@gmail.com>
2022-12-05 16:43:41 +0300

    quiz atempt-pending 2 more

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 
 //quiz1
-
-let num1 = 31 ;
-let num2 = 2;
-multiply = (num1) * (num2);
+let multiply = (num1,num2) =>{
+// const num1 =(Math.random()*10)+1
+// num2 = Number.isInteger(62%num1)
+let  multiply  = 62
+return   Number.parseInt(num1 * num2);
+}
+console.log(31,2)
 
 //quiz2
 
- random = Math.floor(Math.random()* 10 )+1
+ const random = Math.floor(Math.random()* 10 )+1
 
  
  //quiz3
- num3 = 9;
-
- mod = num3 % 5;
+const num3 = 9;
+ let mod = num3 % 5;
+ 
 
  //quiz4
 
- max = Math.max(20);
+ const max = Math.max(20);
 
  
